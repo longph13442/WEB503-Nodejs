35bc068a0fd05ac4a512b0675d495693126b011f
longtvph13442 <longtvph13442@fpt.edu.vn>
2022-03-26 20:58:58 +0700

    b5

--- Controllers/category.js
+++ Controllers/category.js
@@ -1,10 +1,11 @@
-import category from "../model/category";
+import mongoose from "mongoose";
+import category from "../model/category"
 
 export const create= async (req,res) =>{
     console.log(req.body);
     try {
-        const category = await new Category(req.body).save()
-        res.json(category);
+        const categorys = await new category(req.body).save();
+        res.json(categorys);
     } catch (error) {
         res.status(400).json({
             error
